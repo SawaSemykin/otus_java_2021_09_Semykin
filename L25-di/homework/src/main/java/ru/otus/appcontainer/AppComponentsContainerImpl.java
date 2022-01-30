da73e8341ed5ee7c392e3bc292693aa78dab14ae
sawasemykin <sawasemykin@gmail.com>
2022-01-30 12:32:08 +0300

    hw25-di: base case impl

--- L25-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L25-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,22 +1,32 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
+import ru.otus.reflection.ReflectionHelper;
 
+import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
+    private final Object appConfig;
 
     public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+        appConfig = ReflectionHelper.instantiate(initialConfigClass);
         processConfig(initialConfigClass);
     }
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+
+        Arrays.stream(configClass.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order()))
+                .forEachOrdered(this::processConfigMethod);
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -25,13 +35,46 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
+    private void processConfigMethod(Method method) {
+        Object[] dependencies = Arrays.stream(method.getParameterTypes())
+                .map(this::getAppComponent)
+                .toArray();
+        Object component = ReflectionHelper.callMethod(appConfig, method, dependencies);
+        appComponentsByName.put(method.getAnnotation(AppComponent.class).name(), component);
+        appComponents.add(component);
+    }
+
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        if (componentClass.isInterface()) {
+            return (C) getAppComponentByInterface(componentClass);
+        } else {
+            return (C) getAppComponentByClass(componentClass);
+        }
+    }
+
+    private Object getAppComponentByInterface(Class<?> componentInterface) {
+        List<Object> componentCandidates = appComponents.stream()
+                .filter(c -> Arrays.stream(c.getClass().getInterfaces()).anyMatch(i -> i == componentInterface))
+                .collect(Collectors.toList());
+        if (componentCandidates.isEmpty()) {
+            throw new RuntimeException("Component not found");
+        }
+        if (componentCandidates.size() > 1) {
+            throw new RuntimeException("Found several component candidates matched to parameter type. Use more strict query");
+        }
+        return componentCandidates.get(0);
+    }
+
+    private Object getAppComponentByClass(Class<?> componentClass) {
+            return appComponents.stream()
+                    .filter(c -> c.getClass() == componentClass)
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Component not found"));
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
     }
 }
